62a6c0b677f60e51891a4f9d8ca121e3958cd9c3
Juan Manuel Fernandez <juanmanuelunlu@gmail.com>
2023-09-19 20:13:41 -0300

    Se modularizan los resultados.

--- code/funciones_scraping.py
+++ code/funciones_scraping.py
@@ -1,8 +1,117 @@
 # -*- coding: utf-8 -*-
 
 from datetime import datetime
+import os
+from bs4 import BeautifulSoup # Módulos para los requests
+import requests # Módulos para los requests
+import pandas as pd # Módulo para trabajar con df
+import time # Módulos para randomizar los requests
+import random # Módulos para randomizar los requests
+import urllib.parse # Importo el parser para encodear el query
+
+def scraping(query_user, resultados_pedidos, proxies=None):
+    """
+    Esta función es la que realiza el scraping
+    Parameters
+    ----------
+    query_user : str
+        Query con el que se generan los resultados
+    resultados_pedidos : int
+        Cantidad de resultados solicitados
+    proxies : list
+        Proxies válidos para las solicitudes requests.
+
+    Returns
+    -------
+    df_info : dataframe
+        dataframe con los pdfs encontrados y algunos datos
+    resultados_relevantes : int
+        Cantidad de resultados encontrados (sin discriminar pdf)
+    paginas_google : int
+        Cantidad de páginas encontradas
+
+    """
+    # Se inicializan contadores y estructuras de datos
+    start = 1
+    resultados_pdf = 0
+    resultados_relevantes = 0
+    paginas_google = 1
+    item_dict = {}
+    data_list = []
+    
+    query_encoding = urllib.parse.quote(query_user) # Encoding del query
+    
+    while start < resultados_pedidos:
+    
+        # Se preparan los headers y la url request "ambiente"
+        headers = {'User-Agent':'Mozilla/6.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+        url = f'https://scholar.google.es/scholar?start={start}&q={query_encoding}&hl=es&as_sdt=0'
+        
+        # Se muestra el número de página y se incrementa en 1
+        print(f'\nResultados de la página {paginas_google} que poseen paper en PDF: ')
+        paginas_google+=1
+    
+        # Elegir aleatoriamente un proxy de la lista
+        # proxy_elegido = random.choice(proxies)
+    
+        # Se hace el request y se formatea
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.content,'html.parser')
+        
+        for item in soup.select('[data-lid]'): 
+    
+            try: 
+                
+                item_dict = {'titulo': item.select('h3')[0].get_text(),
+                             'autor/origen':  item.select('.gs_a')[0].get_text(),
+                             'enlace': item.select('a')[0]['href'],
+                             'previa_texto': item.select('.gs_rs')[0].get_text()}
+     
+                resultados_relevantes+=1
+                
+                # Verifico si está el paper en pdf
+                if "[PDF]" in item_dict['titulo']:
+                    
+                    # Si es así lo muestro
+                    print('----------------------------------------')
+                    print(item.select('h3')[0].get_text())
+                    
+                    # Si es un pdf además se agrega el diccionario a la lista
+                    data_list.append(item_dict)
+                    
+                    # Se incrementa el contador de resultados positivos
+                    resultados_pdf+=1
+    
+            except Exception as e: 
+                #raise e 
+                print(e)
+        
+        # Se pasan a los siguientes 10 resultados (Scholar muestra 10 p/página)
+        start+=10
+        
+        if start < resultados_pedidos:
+            # Hago una pausa con intervalo random ("3-20")
+            N_seconds = random.randint(3, 20)
+            print(f'\nEl modelo esperará {N_seconds} segundos.\n')
+        
+            time.sleep(N_seconds)
+        
+        # Convertir la lista de diccionarios en un DataFrame
+        df_info = pd.DataFrame(data_list)
+        
+    return df_info, resultados_relevantes, paginas_google
+
+
 
 def fecha_hora_actual_str():
+    """
+    Se genera la fecha y hora actual en un str para incorporar como nombre de archivo/directorio
+
+    Returns
+    -------
+    fecha_hora_str : str
+    """
+
     # Obtén la fecha y hora actual
     fecha_hora_actual = datetime.now()
     
@@ -12,10 +121,27 @@ def fecha_hora_actual_str():
     # Retorna la fecha y hora actual en formato de cadena
     return fecha_hora_str
 
-import os
-import requests
+
 
 def descargar_pdfs(urls, directorio, query_busqueda):
+    """
+    Se descargan los pdf de los resultados del scraping
+
+    Parameters
+    ----------
+    urls : list
+        Lista de urls de pdfs
+    directorio : str
+        String donde se generará el directorio en el que guardan los pdfs
+    query_busqueda : str
+        query utilizado en la búsqueda de resultados
+
+    Returns
+    -------
+    directorio_busqueda : str
+        Directorio donde se guardaron los pdfs
+
+    """
 
     # Crear el directorio de destino si no existe
     if not os.path.exists(directorio):
--- code/scraping.py
+++ code/scraping.py
@@ -1,26 +1,10 @@
 # -*- coding: utf-8 -*-
 
 # Módulos propios que generan funciones para el scraping
-from funciones_scraping import descargar_pdfs
-# from proxy_functions import fetch_proxies_freeproxy, fetch_proxies_sslproxies, get_valid_proxies
+from funciones_scraping import descargar_pdfs, scraping
 from funciones_text import pdf_to_text
 from config import CANTIDAD_RESULTADOS, QUERY, DIRECTORIO_DESTINO
-
-# Módulos para los requests
-from bs4 import BeautifulSoup
-import requests
-
-# Módulo para trabajar con df
-import pandas as pd
-
-# Módulos para randomizar los requests
-import time
-import random
-
-# Importo el parser para encodear el query
-import urllib.parse
-
-query_encoding = urllib.parse.quote(QUERY) # Encoding del query
+# from proxy_functions import fetch_proxies_freeproxy, fetch_proxies_sslproxies, get_valid_proxies
 
 # Se inicializan los proxys libres que se hacen para no ser banneados en el request
 # proxies_list_ssl = fetch_proxies_sslproxies(30)
@@ -28,78 +12,18 @@ query_encoding = urllib.parse.quote(QUERY) # Encoding del query
 # proxies_validos = get_valid_proxies(proxies_list_ssl)
 # proxies_validos.extend(get_valid_proxies(proxies_list_freeproxies))
 
-# Se inicializan contadores y estructuras de datos
-start = 1
-resultados_pdf = 0
-pagina_google = 1
-item_dict = {}
-data_list = []
+data, resultados, paginas_recorridas = scraping(QUERY, CANTIDAD_RESULTADOS)
 
-while start < CANTIDAD_RESULTADOS:
-
-    # Se preparan los headers y la url request "ambiente"
-    headers = {'User-Agent':'Mozilla/6.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
-    url = f'https://scholar.google.es/scholar?start={start}&q={query_encoding}&hl=es&as_sdt=0'
-    
-    # Se muestra el número de página y se incrementa en 1
-    print(f'\nResultados de la página {pagina_google} que poseen paper en PDF: ')
-    pagina_google+=1
-
-    # Elegir aleatoriamente un proxy de la lista
-    # proxy_elegido = random.choice(proxies_validos)
-
-    # Se hace el request y se formatea
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.content,'html.parser')
-    
-    for item in soup.select('[data-lid]'): 
-
-        try: 
-            
-            item_dict = {'titulo': item.select('h3')[0].get_text(),
-                         'autor/origen':  item.select('.gs_a')[0].get_text(),
-                         'enlace': item.select('a')[0]['href'],
-                         'previa_texto': item.select('.gs_rs')[0].get_text()}
- 
-            # Verifico si está el paper en pdf
-            if "[PDF]" in item_dict['titulo']:
-                
-                # Si es así lo muestro
-                print('----------------------------------------')
-                print(item.select('h3')[0].get_text())
-                
-                # Si es un pdf además se agrega el diccionario a la lista
-                data_list.append(item_dict)
-                
-                # Se incrementa el contador de resultados positivos
-                resultados_pdf+=1
-
-        except Exception as e: 
-            #raise e 
-            print(e)
-    
-    # Se pasan a los siguientes 10 resultados (Scholar muestra 10 p/página)
-    start+=10
-    
-    if start < CANTIDAD_RESULTADOS:
-        # Hago una pausa con intervalo random ("3-20")
-        N_seconds = random.randint(3, 20)
-        print(f'\nEl modelo esperará {N_seconds} segundos.\n')
-    
-        time.sleep(N_seconds)
-        
 # Se realiza un resumen:
 print('\n----------------------------------------\n')
 print(f'Query realizado: {QUERY}.')
-print(f'Cantidad de páginas de Google recorridas: {pagina_google}.')
-print(f'Cantidad de artículos relevantes en PDF encontrados: {resultados_pdf}.')
-print(f'Cantidad de artículos relevantes totales pedidos/encontrados: {CANTIDAD_RESULTADOS}/{start}.')
+print(f'Cantidad de páginas de Google recorridas: {paginas_recorridas}.')
+print(f'Cantidad de artículos relevantes en PDF encontrados: {len(data)}.')
+print(f'Cantidad de artículos relevantes totales pedidos/encontrados: {CANTIDAD_RESULTADOS}/{resultados}.')
 
-# Convertir la lista de diccionarios en un DataFrame
-df_info = pd.DataFrame(data_list)
 
 # Una vez procesados los resultados se descargan los pdf
-urls_pdf = df_info['enlace']
+urls_pdf = data['enlace']
 
 # Llamar a la función para descargar los PDFs
 directorio_generado = descargar_pdfs(urls_pdf, DIRECTORIO_DESTINO, QUERY)
@@ -108,5 +32,5 @@ directorio_generado = descargar_pdfs(urls_pdf, DIRECTORIO_DESTINO, QUERY)
 pdf_to_text(directorio_generado)
 
 # Se guardan en un xlsx y un csv
-df_info.to_excel(f'{directorio_generado}/resultados-excel.xlsx')
-df_info.to_csv(f'{directorio_generado}/resultados-csv.csv')
+data.to_excel(f'{directorio_generado}/resultados-excel.xlsx')
+data.to_csv(f'{directorio_generado}/resultados-csv.csv')
